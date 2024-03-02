039deea326c14dd690c6e4bf7d557cb5d2d362a7
Oscar <oskrf13@gmail.com>
2024-03-02 16:57:58 -0500

    xops

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Any, Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from Bot.bot import lifespan,ptb
 from Bot.commands import start
 
@@ -17,8 +17,9 @@ async def read_root():
 
 
 @app.post("/webhook",status_code=200)
-async def webhook(val:Any):
-    print(val)
+async def webhook(val:Request):
+    data = await val.json()
+    print(data)
     return {"ok": True}
 
 
